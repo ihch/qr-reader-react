22c5b6574e6036b6361c111b37b3f6aaa794ac98
ihch <ryubi.102395@gmail.com>
2020-04-19 02:48:48 +0900

    Updates

---
+++ precache-manifest.55d258947e0703a492e334b44802c42e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7dd3dce6d4517083b804dbc8c612702",
+    "url": "/ihch.github.io/qr-reader-react/index.html"
+  },
+  {
+    "revision": "c1cf0cfed42a20dc8bfd",
+    "url": "/ihch.github.io/qr-reader-react/static/css/main.b56b6c51.chunk.css"
+  },
+  {
+    "revision": "6cb5a7002b52b45e101e",
+    "url": "/ihch.github.io/qr-reader-react/static/js/2.429f186c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ihch.github.io/qr-reader-react/static/js/2.429f186c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1cf0cfed42a20dc8bfd",
+    "url": "/ihch.github.io/qr-reader-react/static/js/main.25aa2bdd.chunk.js"
+  },
+  {
+    "revision": "2d05773f497a866375e3",
+    "url": "/ihch.github.io/qr-reader-react/static/js/runtime-main.aba1229e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fa70839f7636827756e34f06fa166865.js"
+  "/ihch.github.io/qr-reader-react/precache-manifest.55d258947e0703a492e334b44802c42e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ihch.github.io/qr-reader-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
