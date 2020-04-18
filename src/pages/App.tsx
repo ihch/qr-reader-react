1fa6179c77cff935a40dedab641a61bd6e8b3988
nemusou <ryubi.102395@gmail.com>
2020-04-19 02:09:06 +0900

    feat qr-readerを搭載

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from '../static/logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import QrReader from 'react-qr-reader'
 
-function App() {
+const App: React.FC<{}> = () => {
+  const [result, setResult] = useState('')
+  const handleScan = (scanResult: string) => scanResult && setResult(scanResult);
+  const handleError = (err: Error) => console.log(err);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <QrReader
+        delay={300}
+        onScan={handleScan}
+        onError={handleError}
+        style={{ width: '100%', height: 'auto' }}
+      />
     </div>
   );
-}
+};
 
 export default App;
